9b9c324c5b38751aec697fd9ca726bd179ec3e31
DamarisCaballero <damariscallero011@gmail.com>
2023-04-19 15:46:47 -0600

    little changes

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,6 +1,7 @@
 const API='https://youtube-v31.p.rapidapi.com/search?channelId=UCw05fUBPwmpu-ehXFMqfdMw&part=snippet%2Cid&order=date&maxResults=9';
 
 const content=null||document.getElementById('content');
+
 const options={
     method:'GET',
     headers:{'X-RapidAPI-Host':'youtube-v31.p.rapidapi.com','X-RapidAPI-Key':'c68edbdba9mshc3cb6eb8038f3b2p1f444ajsna5de19fe1256'}};
@@ -9,12 +10,13 @@ const options={
     (async()=>{
     try{
         const videos=await fetchData(API);
-        let view=`
+        let view = `
     ${videos.items.map(video=>`
       <div class="group relative">
         <div
           class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-          <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+          <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" 
+          class="w-full">
         </div>
         <div class="mt-4 flex justify-between">
           <h3 class="text-sm text-gray-700">
@@ -26,7 +28,9 @@ const options={
 
     `).slice(0,4).join('')}
 
-    `;}catch{
-
+    `;
+        content.innerHTML = view;
+}catch (error) {
+    console.log(error);
     }
 })();
